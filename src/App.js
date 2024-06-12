aea31265ce43ec869eb9f070f1999a16c1e4a5a5
Upadhyay B <upadhyay.b@dgsl.in>
2024-06-12 20:37:48 +0530

    centralizing an element

--- src/App.js
+++ src/App.js
@@ -4,41 +4,31 @@ import "./App.css";
 import Notes from "./components/Notes";
 
 function App() {
-  const [myName, setMyName] = useState("Aryan");
-
-  const notes = [
-    { name: "helloWorld", key: "sgdhwegdw" },
-    { name: "Dishes" },
-    { name: "AryanWorld" },
-  ];
-
-  const changeName = () => {
-    if (myName === "Aryan") {
-      setMyName("Shubham");
-    } else {
-      setMyName("Aryan");
-    }
-  };
-
+  const onSubmit = () => {};
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>{myName}</h1>
-        <button onClick={changeName}>Change Name</button>
-        <Notes notes={notes} myName={myName} />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className="App"
+      style={{
+        height: "100vh",
+        border: "2px solid red",
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <form
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          width: "200px",
+          border: "2px solid blue",
+        }}
+      >
+        <input placeholder="email" type="text" />
+        <input placeholder="password" type="password" />
+        <button>Submit</button>
+      </form>
     </div>
   );
 }
