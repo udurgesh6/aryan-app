7ebcb94d94153241072bf837173ee8b6bd49245d
Upadhyay B <upadhyay.b@dgsl.in>
2024-06-12 21:12:04 +0530

    controlled component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import { useState } from "react";
-import logo from "./logo.svg";
 import "./App.css";
-import Notes from "./components/Notes";
 
 function App() {
-  const onSubmit = () => {};
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [age, setAge] = useState(0);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    // do the job
+  };
+
   return (
     <div
       className="App"
@@ -24,10 +30,38 @@ function App() {
           width: "200px",
           border: "2px solid blue",
         }}
+        onSubmit={onSubmit}
       >
-        <input placeholder="email" type="text" />
-        <input placeholder="password" type="password" />
-        <button>Submit</button>
+        <input
+          placeholder="email"
+          type="text"
+          value={email}
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+        />
+        <input
+          placeholder="password"
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <input
+          placeholder="age"
+          type="number"
+          value={age}
+          onChange={(e) => {
+            if (e.target.value > 100) {
+              alert("Value can't be greater than 100 !");
+            } else {
+              setAge(e.target.value);
+            }
+          }}
+          min={0}
+          max={100}
+        />
+        <input type="checkbox" />
+        <button type="submit">Submit</button>
       </form>
     </div>
   );
