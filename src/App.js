599e34ae4a5ab680b2b0826ce83670d2c59dcbba
Upadhyay B <upadhyay.b@dgsl.in>
2024-06-16 21:10:10 +0530

    useEffect details

--- src/App.js
+++ src/App.js
@@ -1,122 +1,16 @@
 import { useState } from "react";
 import "./App.css";
+import Notes from "./components/Notes";
 
 function App() {
-  const [notes, setNotes] = useState([]);
-  const [updatingNote, setUpdatingNote] = useState(false);
-
-  const [note, setNote] = useState({
-    name: "",
-    createdBy: "",
-    description: "",
-    education: [],
-    previousJob: {
-      companYName: "",
-      salary: "",
-      years: "",
-    },
-  });
-
-  const onCreate = (e) => {
-    e.preventDefault();
-    const totalNotes = notes.length;
-    setNotes([
-      ...notes,
-      {
-        ...note,
-        createdAt: new Date().toDateString(),
-        index: totalNotes,
-      },
-    ]);
-    setNote({
-      name: "",
-      createdBy: "",
-      description: "",
-    });
+  const [showNote, setShowNote] = useState(false);
+  const changeShowOfNote = () => {
+    setShowNote(!showNote);
   };
-
-  const changeTHisNote = (note) => {
-    setUpdatingNote(true);
-    setNote(note);
-  };
-
-  const updateNote = (e) => {
-    e.preventDefault();
-    const tempNotes = [...notes];
-    tempNotes[note.index] = note;
-    setNotes(tempNotes);
-    setNote({
-      name: "",
-      createdBy: "",
-      description: "",
-    });
-    setUpdatingNote(false);
-  };
-
-  const deleteNote = (ind) => {
-    const tempNotes = [...notes];
-    tempNotes.splice(ind, 1);
-    setNotes(tempNotes);
-  };
-
   return (
-    <div
-      className="App"
-      style={{
-        height: "100vh",
-        border: "2px solid red",
-        display: "flex",
-        flexDirection: "column",
-        overflowY: "auto",
-      }}
-    >
-      <h1>MODE: {updatingNote ? "UPDATE" : "CREATE"}</h1>
-      <form
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          width: "200px",
-          border: "2px solid blue",
-        }}
-        onSubmit={updatingNote ? updateNote : onCreate}
-      >
-        <input
-          type="text"
-          value={note.name}
-          onChange={(e) => setNote({ ...note, name: e.target.value })}
-          placeholder="Name"
-        />
-        <input
-          type="description"
-          value={note.description}
-          onChange={(e) => setNote({ ...note, description: e.target.value })}
-          placeholder="Description"
-        />
-        <input
-          type="type"
-          value={note.createdBy}
-          onChange={(e) => setNote({ ...note, createdBy: e.target.value })}
-          placeholder="Created By"
-        />
-        <button type="submit">{updatingNote ? "Update" : "Create"}</button>
-      </form>
-
-      <div style={{ display: "flex", flexDirection: "column" }}>
-        <h2>My Notes:</h2>
-        {notes.map((note, noteIndex) => (
-          <div
-            style={{ display: "flex", flexDirection: "column", width: "300px" }}
-            key={note.createdAt + noteIndex}
-          >
-            <p>Title: {note.name}</p>
-            <p>Created At: {note.createdAt}</p>
-            <p>Created By: {note.createdBy}</p>
-            <p>Description: {note.description}</p>
-            <button onClick={() => changeTHisNote(note)}>Edit</button>
-            <button onClick={() => deleteNote(note.index)}>Delete Note</button>
-          </div>
-        ))}
-      </div>
+    <div>
+      <div>{showNote && <Notes />}</div>
+      <button onClick={changeShowOfNote}>Show/Disable Note</button>
     </div>
   );
 }
--- src/components/Notes.js
+++ src/components/Notes.js
@@ -1,20 +1,48 @@
-const Notes = ({ notes, myName }) => {
-  const calculatesStringLength = () => {
-    let totalLength = 0;
-    notes.forEach((note) => {
-      totalLength += note.length;
-    });
-    console.log(totalLength);
-  };
-  calculatesStringLength();
+import { useEffect, useState } from "react";
+
+const Notes = () => {
+  const [showNoteHeader, setShowNoteHeader] = useState(true);
+  const [showNoteDesc, setShowNoteDesc] = useState(true);
+
+  // Case 1: This useffect hook will run on the first mount and on each and every state updates
+  // useEffect(() => {
+  //   console.log("I am getting mounted!");
+  // });
+
+  // Case2:  This useffect hook will run on the first mount but not on any state updates
+  // useEffect(() => {
+  //   console.log("I am getting mounted!");
+  // }, []);
+
+  // Case 3: This useffect hook will run on the first mount but on only on state updates which are inside the dependency array
+  // useEffect(() => {
+  //   console.log("I am getting mounted!");
+  // }, [showNoteDesc]);
+
+  useEffect(() => {
+    console.log("I am getting mounted!");
+    return () => {
+      console.log("I am getting unmounted!");
+    };
+  }, []);
+
+  // useEffect(() => {
+  //   first
+  //   return () => {
+  //     second
+  //   }
+  // }, [third])
+
   return (
     <div style={{ border: "1px solid red" }}>
-      <h1>Notes from {myName}:</h1>
-      {notes.map((note, noteIndex) => (
-        <div key={noteIndex}>
-          <p>{note.name}</p>
-        </div>
-      ))}
+      {showNoteHeader && <h1>NOte</h1>}
+      {showNoteDesc && <p>I am note component</p>}
+      <button onClick={() => setShowNoteHeader(!showNoteHeader)}>
+        Show/Disable Header
+      </button>
+      <button onClick={() => setShowNoteDesc(!showNoteDesc)}>
+        Show/Disable Desc
+      </button>
     </div>
   );
 };
