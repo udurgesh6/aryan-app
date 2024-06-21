61e010013c13023cefcd0ccff7b888274dc73af6
Upadhyay B <upadhyay.b@dgsl.in>
2024-06-21 21:24:06 +0530

    usecase 2 for useEFfect

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
 import { useState, useEffect } from "react";
+import axios from "axios";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(1);
-
-  const incrementCount = () => {
-    setCount(count + 1);
-  };
+  const [post, setPost] = useState(null);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-    console.log(count);
-    if (count % 2 === 0) {
-      alert(`Hello the count is ${count}`);
-    }
-  }, [1]);
+    setLoading(true);
+    axios
+      .get("https://jsonplaceolder.typicode.com/todos/1")
+      .then((response) => {
+        console.log(response);
+        //
+        //
+        //
+        //
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+      .finally(() => {
+        console.log("Done");
+        setLoading(false);
+      });
+  }, []);
 
   return (
     <div>
-      <p>{count}</p>
-      <button onClick={incrementCount}>Increment</button>
+      <p>Status: {loading ? "Loading..." : "Done"}</p>
+      <h1>Hello World</h1>
+      <p>{post?.title}</p>
     </div>
   );
 }
