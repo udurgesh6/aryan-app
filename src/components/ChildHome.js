cfbbad9a8fed17f51ed2a65076433b9afd63f32c
Upadhyay B <upadhyay.b@dgsl.in>
2024-07-02 11:13:12 +0530

    memo

--- src/components/ChildHome.js
+++ src/components/ChildHome.js
@@ -1,7 +1,7 @@
 import React, { memo } from "react";
 
 const ChildHome = ({ handleClick }) => {
-  console.log("hello world I am ChildHome Component");
+  console.log("I am ChildHome Component");
 
   return (
     <>
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,39 +1,24 @@
-import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import React, { useRef, useState } from "react";
+import ChildHome from "./ChildHome";
 
-const App = ({ cartItems, setCartItems }) => {
-  const [id] = useEffect();
-  // this is my current product count
-  const [currentProductCount, setCurrentProductCount] = useState(0);
-  const [currentProductDetail, setCurrentProductDetail] = useState(null);
+const App = () => {
+  console.log("I am Parent");
 
-  useEffect(() => {
-    // logic to get the count of current product from cartItems
-    // cartItems ==>  [{id:1, name:"test1", count: 1},{id:2, name:"test2", count: 2},{id:3, name:"test3", count: 4}]
-    const currentProductDetail = cartItems.filter((item) => item.id === id)[0]; // {id:1, name:"test1", count: 1}
-    setCurrentProductCount(currentProductDetail.count);
-  }, [cartItems]);
+  const nameRef = useRef();
 
-  const addItemToCart = () => {
-    const tempCartItems = [...cartItems];
+  const [count, setCount] = useState(0);
 
-    const indexOfCurrentProduct = cartItems.findIndex((item) => item.id === id);
+  const [name, setName] = useState("");
 
-    if (indexOfCurrentProduct === -1) {
-      // this product is not yet present inside the cart
-      setCartItems([...cartItems, { ...currentProductDetail, count: 1 }]);
-    } else {
-      // this item is already present, just increment the count by 1
-      tempCartItems[indexOfCurrentProduct].count += 1;
-      setCartItems(tempCartItems);
-    }
+  const handleClick = () => {
+    setCount(count + 1);
   };
 
   return (
     <div className="flex flex-col">
-      <p>+ {currentProductCount} -</p>
+      <input className="border-2" ref={nameRef} />
+      <ChildHome handleClick={handleClick} />
     </div>
   );
 };
-
 export default App;
